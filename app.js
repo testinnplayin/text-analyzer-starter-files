e3429229f9d19892600028c179b78bcda3001607
Rachelle <archaurore@gmail.com>
2016-11-08 17:18:54 -0500

    created word count function bound to button click in app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// your code here!
+function cleaner(text) { //cleans up given test text
+	return text.toLowerCase().replace(/\W[^a-z']/g, ' ').split(' ');
+}
+
+function calcLength(arr) {
+	return arr.length;
+}
+
+function handleSubmit() {
+	$('button[type="submit"]').click(function(e) {
+		e.preventDefault();
+		var crudeText, cleanedText;
+
+		crudeText = $('#user-text').val();
+		cleanedText = cleaner(crudeText);
+
+		$('main dl').removeClass('hidden');
+
+		$('.js-word-count').empty().append("<dd>" + calcLength(cleanedText) + "</dd>");
+	});
+}
+
+$(document).ready(handleSubmit);
