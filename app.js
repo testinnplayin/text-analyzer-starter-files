01386b6e3a9b3cf906123e196673109e70c7c3ca
Rachelle <archaurore@gmail.com>
2016-11-09 12:53:38 -0500

    created word average length function in app.js

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 function cleaner(text) { //cleans up given test text
-	return text.toLowerCase().replace(/\W[^a-z']/g, ' ').replace(/\./g, '').split(' ');
+	return text.toLowerCase().replace(/\W[^a-z']/g, ' ').replace(/\./g, '').split(' '); //second replace added because of end period not being selected
 }
 
-function calcLength(arr) {
-	return arr.length;
-}
-
-function calcUniq(arr) {
+function createWordObj(arr) {
 	var wordFreq = {};
 
 	arr.forEach(function(word) {
 		wordFreq[word] = (wordFreq || 0) + 1;
 	});
 
+	return wordFreq;
+}
+
+function calcLength(arr) {
+	return arr.length;
+}
+
+function calcUniq(wordFreq) {
 	return Object.keys(wordFreq).length;
 }
 
+function calcAvWordLng(wordFreq) {
+	var wordLngArr = [];
+	var words = Object.keys(wordFreq);
+	var lng = words.length;
+
+	for (var word of words) {
+		wordLngArr.push(word.length);
+	}
+
+	return wordLngArr.reduce(function(a, b) {
+		return (a + b);
+	}) / lng;
+	
+}
 
 function displayData(select, f) {
 	$(select).empty().append("<dd>" + f + "</dd>");
@@ -24,16 +42,19 @@ function displayData(select, f) {
 function handleSubmit() {
 	$('button[type="submit"]').click(function(e) {
 		e.preventDefault();
-		var crudeText, cleanedText;
+		
+		var crudeText, cleanedText, wordObj;
 
 		crudeText = $('#user-text').val();
 		cleanedText = cleaner(crudeText);
+		wordObj = createWordObj(cleanedText);
 
 		$('main dl').removeClass('hidden');
 
 		var displayObj = {
 			'.js-word-count' : calcLength(cleanedText),
-			'.js-uniq-cnt' : calcUniq(cleanedText)
+			'.js-uniq-cnt' : calcUniq(wordObj),
+			'.js-word-lng' : calcAvWordLng(wordObj)
 		};
 
 		for (var selector in displayObj) {
