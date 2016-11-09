d470786fc56ec3c6cf868e510cbbe42f84589b1f
Rachelle <archaurore@gmail.com>
2016-11-09 15:07:39 -0500

    created average sentence length function in app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 function cleaner(text) { //cleans up given test text
-	return text.toLowerCase().replace(/\W[^a-z']/g, ' ').replace(/\./g, '').split(' '); //second replace added because of end period not being selected
+	return text.toLowerCase().replace(/[\W[^a-z']]/g, ' ').replace(/\./g, '').split(' '); //second replace added because of end period not being selected
 }
 
-function createWordObj(arr) {
+//creation functions
+
+function createWordObj(arr) { //creates wordFreq object that contains words and their frequencies
 	var wordFreq = {};
 
 	arr.forEach(function(word) {
@@ -12,15 +14,17 @@ function createWordObj(arr) {
 	return wordFreq;
 }
 
-function calcLength(arr) {
+//calculation functions
+
+function calcLength(arr) { //calculate length of text
 	return arr.length;
 }
 
-function calcUniq(wordFreq) {
+function calcUniq(wordFreq) { //calculate amount of unique words in text
 	return Object.keys(wordFreq).length;
 }
 
-function calcAvWordLng(wordFreq) {
+function calcAvWordLng(wordFreq) { //calculate average word length
 	var wordLngArr = [];
 	var words = Object.keys(wordFreq);
 	var lng = words.length;
@@ -31,18 +35,37 @@ function calcAvWordLng(wordFreq) {
 
 	return wordLngArr.reduce(function(a, b) {
 		return (a + b);
-	}) / lng;
-	
+	}) / lng;	
 }
+
+function calcAvSent(str) { //calculate average sentence length
+	var sentences, lng, sentLng, sum;
+
+	sentences = str.split(/[!?.]/g);
+	lng = sentences.length;
+	sum = 0;
+
+	for (var i = 0; i < lng - 1; i++) {
+		sentLng = sentences[i].trim().split(' ').join('').length;
+		sum += sentLng;
+		console.log(sum);
+	}
+
+	return sum / (lng - 1);
+}
+
+//display functions
 
 function displayData(select, f) {
 	$(select).empty().append("<dd>" + f + "</dd>");
 }
 
+//event handlers
+
 function handleSubmit() {
 	$('button[type="submit"]').click(function(e) {
 		e.preventDefault();
-		
+
 		var crudeText, cleanedText, wordObj;
 
 		crudeText = $('#user-text').val();
@@ -54,7 +77,8 @@ function handleSubmit() {
 		var displayObj = {
 			'.js-word-count' : calcLength(cleanedText),
 			'.js-uniq-cnt' : calcUniq(wordObj),
-			'.js-word-lng' : calcAvWordLng(wordObj)
+			'.js-word-lng' : calcAvWordLng(wordObj),
+			'.js-sent-lng' : calcAvSent(crudeText)
 		};
 
 		for (var selector in displayObj) {
